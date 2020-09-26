3531edc2891ff29e13ae3f19b8870ae1bcebca8d
KingKimani <53226753+KingKimani@users.noreply.github.com>
2020-09-26 23:13:41 +0300

    completion of Front end but some functionality is still blank and starting JS
    completed

---
+++ Gaming/website/Kinggaming Website/page/product.php
@@ -0,0 +1,307 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <title>Serves Web Tech Solution and Services</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <style>
+
+  h1.header1{
+      color: #660000;
+  }
+  h1.header2{
+    background-color: #0000cc;
+    color: #FFF;
+  }
+  body{
+
+    font-family: Verdana, Geneva, sans-serif;
+  }
+  .navbar {
+      margin-bottom: 0;
+      border-radius: 0;
+      background-color: #0000cc;
+      color: #FFF;
+      padding: 1% 0%;
+      font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol";
+      font-size: 1.3em;
+      border: 0;
+  }
+  .left{
+
+    float: left;
+  }
+
+
+  .navbar-inverse .navbar-nav .active a, .navbar-inverse .navbar-nav .active a:focus, .navbar-inverse .navbar-nav {
+    color: #FFF;
+    background-color: #0000cc;
+
+
+
+
+  }
+
+
+
+  .navbar-inverse .navbar-nav li a{
+
+    color: #D5D5D5;
+  }
+  .carousel-caption{
+    top: 50%;
+    transform: translateY(-50%);
+    text-transform: uppercase;
+  }
+  .btn{
+    font-size: 18px;
+    color: #D3D3D3;
+    padding: 12px 22px;
+    background-color: orange;
+    border: 2px solid #FFF;
+  }
+  .container{
+    margin: 4% auto;
+  }
+  #icon{
+    max-width: 200px;
+    margin: 1% auto;
+  }
+  footer{
+    width: 100%;
+    background-color: #0000cc;
+    padding: 5%;
+    color: #FFF;
+  }
+  .fa{
+    padding: 15px;
+    font-size: 25px;
+    color: #FFF;
+  }
+  .fa:hover{
+    color: #D5D5D5;
+    text-decoration: none;
+
+  }
+  .fa-facebook{
+    color: #365899;
+  }
+  .fa-twitter{
+
+    color: #1da1f2
+  }
+  .color{
+      color: #0000cc;
+      font-size: 5rem;
+  }
+  div.line{
+    background-color: #0000cc;
+  }
+
+
+
+
+
+  @media (max-width: 900px){
+   .fa{
+      font-size: 20px;
+      padding: 10px;
+   }
+  }
+
+
+  @media (max-width: 600px){
+    .carousel-caption{
+      display: none;
+    }
+    #icon{
+      max-width: 150px;
+    }
+    h4{
+      font-size: 1.7em;
+    }
+
+  }
+
+
+  </style>
+</head>
+<body>
+  <nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#MyNavbar">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+
+      </div>
+
+      <div class="collapse navbar-collapse" id="MyNavbar">
+
+
+
+        <a class="left" href="home.php"><img src="img/serve.png"></a>
+          <ul class="nav navbar-nav navbar-left">
+            <li class="active"><a href="home.php">Home</a></li>
+            <li class="active"><a href="management.php">Management</a></li>
+            <li class="active"><a href="services.php">Services</a></li>
+            <li class="active"><a href="product.php">Products</a></li>
+            <li class="active"><a href="../../../Game_Shop/index.php">Buy Here!</a></li>
+
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li class="active"><a>Contact Us</a></li>
+            <li class="active"><a href="#" class="fa fa-facebook"></a></li>
+            <li class="active"><a href="#" class="fa fa-twitter"></a></li>
+            <li class="active"><a href="#" class="fa fa-google"></a></li>
+          </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!--end slider -->
+  <div class="container text-center">
+    <div class="row">
+      <div>
+      <h1 class="header2">Product</h1>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="container text-center">
+    <div class="row">
+      <div class="col-md-6">
+        <img src="img/MW33.jpg" class="img-responsive">
+      </div>
+      <div class="col-md-6">
+        <h1>Call Of Duty:Mordern Warfare</h1>
+        <h2>The stakes have never been higher as players take on the role of lethal Tier One operators in a heart-racing saga that will affect the global balance of power. Call of Duty®: Modern Warfare® engulfs fans in an incredibly raw, gritty, provocative narrative that brings unrivaled intensity and shines a light on the changing nature of modern war. Developed by the studio that started it all, Infinity Ward delivers an epic reimagining of the iconic Modern Warfare® series from the ground up. </h2>
+        <br>
+        <h3>Available:PS4,X-Box,PC</h3>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-md-6">
+        <img src="img/GOT.jpg" class="img-responsive">
+      </div>
+      <div class="col-md-6">
+        <h1>Ghost of Tsushima</h1>
+        <h2>In the late 13th century, the Mongol empire has laid waste to entire nations along their campaign to conquer the East. Tsushima Island is all that stands between mainland Japan and a massive Mongol invasion fleet led by the ruthless and cunning general, Khotun Khan. As the island burns in the wake of the first wave of the Mongol assault, samurai warrior Jin Sakai stands as one of the last surviving members of his clan.</h2>
+      <br>
+        <h3>Available:PS4,PC</h3>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-md-6">
+        <img src="img/Avengers.jpg" class="img-responsive">
+      </div>
+      <div class="col-md-6">
+        <h1>Marvel Avenger's</h1>
+        <h2>Marvel's Avengers is an action role-playing brawler video game co-developed by Crystal Dynamics and published by Square Enix. Based on the Marvel Comics superhero team the Avengers, the game is mainly inspired by the Marvel Cinematic Universe's iteration of the group, as well as the long-running comic book mythology. </h2>
+      <br>
+        <h3>Available:PS4,X-Box,PC</h3>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-md-6">
+        <img src="img/jfo.jpeg" class="img-responsive">
+      </div>
+      <div class="col-md-6">
+        <h1>Star Wars Jedi: Fallen Order</h1>
+        <h2>Star Wars Jedi: Fallen Order is an action-adventure game developed by Respawn Entertainment and published by Electronic Arts. It was released for Windows, PlayStation 4, and Xbox One on November 15, 2019. A Stadia version is due in 2020.</h2>
+      <br>
+        <h3>Available:PS4,X-Box,PC</h3>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-md-6">
+        <img src="img/NBA.jpg" class="img-responsive">
+      </div>
+      <div class="col-md-6">
+        <h1>NBA 2K21</h1>
+        <h2>NBA 2K21 is a basketball simulation video game that was developed by Visual Concepts and published by 2K Sports, based on the National Basketball Association. It is the 22nd installment in the NBA 2K franchise and the successor to NBA 2K20</h2>
+        <br>
+          <h3>Available:PS4,X-Box</h3>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-md-6">
+        <img src="img/LOU.jpg" class="img-responsive">
+      </div>
+      <div class="col-md-6">
+        <h1>The Last of Us Part II</h1>
+        <h2>The Last of Us Part II is a 2020 action-adventure game developed by Naughty Dog and published by Sony Interactive Entertainment for the PlayStation 4.</h2>
+        <br>
+          <h3>Available:PS4,X-Box</h3>
+      </div>
+    </div>
+
+     <div class="row">
+      <div class="col-md-6">
+        <img src="img/Nioh.jpg" class="img-responsive">
+      </div>
+      <div class="col-md-6">
+        <h1>Nioh 2</h1>
+        <h2>Nioh 2 is an action role-playing game developed by Team Ninja. A prequel to Nioh, the game was released for the PlayStation 4 on March 13, 2020, where it was published by Koei Tecmo in Japan and Sony Interactive Entertainment worldwide.</h2>
+        <br>
+          <h3>Available:PS4,X-Box,PC</h3>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-md-6">
+        <img src="img/FFR.jpg" class="img-responsive">
+      </div>
+      <div class="col-md-6">
+        <h1>Final Fantasy VII Remake</h1>
+        <h2>Final Fantasy VII Remake is a 2020 action role-playing game developed and published by Square Enix. It is the first in a planned series of games remaking the 1997 PlayStation game Final Fantasy VII. Set in the dystopian cyberpunk metropolis of Midgar, it puts players in the role of a mercenary named Cloud Strife. </h2>
+        <br>
+          <h3>Available:PS4,X-Box,PC</h3>
+      </div>
+    </div>
+
+
+  </div>
+
+
+
+  <!--footer-->
+
+  <footer class="container-fluid text-center">
+    <div class="row">
+      <div class="col-sm-6">
+        <h3> Develop By </h3>
+        <br>
+        <h4>Fredrick K. Waitara</h4>
+      </div>
+      <--<div class="col-sm-">
+        <h3>Connect</h3>
+        <a href="#" class="fa fa-facebook"></a>
+        <a href="#" class="fa fa-twitter"></a>
+        <a href="#" class="fa fa-google"></a>
+        <a href="#" class="fa fa-linkedin"></a>
+        <a href="#" class="fa fa-youtube"></a>
+      </div>-->
+      <div class="col-sm-6">
+        <img src="img/ironman2.png" class="icon">
+      </div>
+
+    </div>
+  </footer>
+
+
+</body>
+</html>
